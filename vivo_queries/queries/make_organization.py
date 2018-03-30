4dc9b2f870f0ebee0ab9c3db89321907b8f6b5bb
AjanthaRamineni <ajantha.5779@gmail.com>
2018-03-30 14:01:52 -0400

    Refactoring Make_Organization

--- vivo_queries/queries/make_organization.py
+++ vivo_queries/queries/make_organization.py
@@ -8,9 +8,7 @@ def get_params(connection):
     params = {'Organization': organization}
     return params
 
-
-def run(connection, **params):
-
+def fill_params(connection, **params):
     if params['Organization'].n_number:
         return
 
@@ -18,17 +16,31 @@ def run(connection, **params):
 
     params['Organization'].n_number = connection.gen_n()
     params['Organization'].role = connection.gen_n()
-    # template data into q
-    q = Environment().from_string("""\
+    return params
+
+def get_triples():
+
+    triples = """\
+        <{{upload_url}}{{Organization.n_number}}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Organization> .
+        <{{upload_url}}{{Organization.n_number}}> <http://www.w3.org/2000/01/rdf-schema#label> "{{Organization.name}}" .
+    """
+
+    api_trip = """\
     INSERT DATA {
         GRAPH <http://vitro.mannlib.cornell.edu/default/vitro-kb-2>
-        {
-            <{{upload_url}}{{Organization.n_number}}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Organization> .
-            <{{upload_url}}{{Organization.n_number}}> <http://www.w3.org/2000/01/rdf-schema#label> "{{Organization.name}}" . 
-        }
+        {{
+            {TRIPS}
+        }}
     }
-    """)
+        """.format(TRIPS=triples)
+    trips = Environment().from_string(api_trip)
+    return trips
 
+
+
+def run(connection, **params):
+    params = fill_params(connection, **params)
+    q = get_triples(True)
     # Send data to Vivo
     print('=' * 20 + "\nCreating new organization\n" + '=' * 20)
     response = connection.run_update(q.render(**params))
